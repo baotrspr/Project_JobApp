5f32073cc99a6515cb8b3a8a712ea2eff688691a
baotrspr <xuanbao8987@gmail.com>
2024-02-27 15:36:53 +0700

    update 15:36

--- UC/UCProfile.Designer.cs
+++ UC/UCProfile.Designer.cs
@@ -29,25 +29,244 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblBday = new System.Windows.Forms.Label();
+            this.dtpBday = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxSex = new System.Windows.Forms.ComboBox();
+            this.lblIdentity = new System.Windows.Forms.Label();
+            this.txtIdentity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblMoreInfo = new System.Windows.Forms.Label();
+            this.pbxPhoto = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtxtMoreInfo = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPhoto)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(272, 270);
+            this.label1.Location = new System.Drawing.Point(269, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(229, 55);
             this.label1.TabIndex = 1;
             this.label1.Text = "PROFILE";
             // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(31, 98);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(31, 16);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Ten";
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(145, 96);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(31, 145);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(70, 16);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(145, 143);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 22);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cmnd";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(145, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblBday
+            // 
+            this.lblBday.AutoSize = true;
+            this.lblBday.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBday.Location = new System.Drawing.Point(31, 237);
+            this.lblBday.Name = "lblBday";
+            this.lblBday.Size = new System.Drawing.Size(67, 16);
+            this.lblBday.TabIndex = 2;
+            this.lblBday.Text = "Ngay sinh";
+            // 
+            // dtpBday
+            // 
+            this.dtpBday.CustomFormat = "dd/MM/yyyy";
+            this.dtpBday.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBday.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBday.Location = new System.Drawing.Point(145, 233);
+            this.dtpBday.Name = "dtpBday";
+            this.dtpBday.Size = new System.Drawing.Size(200, 24);
+            this.dtpBday.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gioi tinh";
+            // 
+            // cbxSex
+            // 
+            this.cbxSex.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSex.FormattingEnabled = true;
+            this.cbxSex.Items.AddRange(new object[] {
+            "Nam",
+            "Nu"});
+            this.cbxSex.Location = new System.Drawing.Point(145, 283);
+            this.cbxSex.Name = "cbxSex";
+            this.cbxSex.Size = new System.Drawing.Size(200, 24);
+            this.cbxSex.TabIndex = 5;
+            this.cbxSex.Text = "Choose";
+            // 
+            // lblIdentity
+            // 
+            this.lblIdentity.AutoSize = true;
+            this.lblIdentity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdentity.Location = new System.Drawing.Point(31, 332);
+            this.lblIdentity.Name = "lblIdentity";
+            this.lblIdentity.Size = new System.Drawing.Size(42, 16);
+            this.lblIdentity.TabIndex = 2;
+            this.lblIdentity.Text = "Cmnd";
+            // 
+            // txtIdentity
+            // 
+            this.txtIdentity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIdentity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdentity.Location = new System.Drawing.Point(145, 330);
+            this.txtIdentity.Name = "txtIdentity";
+            this.txtIdentity.Size = new System.Drawing.Size(200, 22);
+            this.txtIdentity.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Hoc van";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dai hoc",
+            "Cao dang",
+            "THPT",
+            "THCS",
+            "TH"});
+            this.comboBox1.Location = new System.Drawing.Point(145, 374);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Choose";
+            // 
+            // lblMoreInfo
+            // 
+            this.lblMoreInfo.AutoSize = true;
+            this.lblMoreInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoreInfo.Location = new System.Drawing.Point(31, 424);
+            this.lblMoreInfo.Name = "lblMoreInfo";
+            this.lblMoreInfo.Size = new System.Drawing.Size(94, 16);
+            this.lblMoreInfo.TabIndex = 2;
+            this.lblMoreInfo.Text = "Thong tin them";
+            // 
+            // pbxPhoto
+            // 
+            this.pbxPhoto.Location = new System.Drawing.Point(569, 96);
+            this.pbxPhoto.Name = "pbxPhoto";
+            this.pbxPhoto.Size = new System.Drawing.Size(200, 200);
+            this.pbxPhoto.TabIndex = 6;
+            this.pbxPhoto.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Chon anh ho so";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // rtxtMoreInfo
+            // 
+            this.rtxtMoreInfo.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.rtxtMoreInfo.Location = new System.Drawing.Point(145, 424);
+            this.rtxtMoreInfo.Name = "rtxtMoreInfo";
+            this.rtxtMoreInfo.Size = new System.Drawing.Size(200, 100);
+            this.rtxtMoreInfo.TabIndex = 8;
+            this.rtxtMoreInfo.Text = "";
+            // 
             // UCProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.rtxtMoreInfo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbxPhoto);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbxSex);
+            this.Controls.Add(this.dtpBday);
+            this.Controls.Add(this.txtIdentity);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMoreInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblIdentity);
+            this.Controls.Add(this.lblBday);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.label1);
             this.Name = "UCProfile";
             this.Size = new System.Drawing.Size(800, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPhoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +275,23 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblBday;
+        private System.Windows.Forms.DateTimePicker dtpBday;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxSex;
+        private System.Windows.Forms.Label lblIdentity;
+        private System.Windows.Forms.TextBox txtIdentity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblMoreInfo;
+        private System.Windows.Forms.PictureBox pbxPhoto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtxtMoreInfo;
     }
 }
