1d5b88d016d7c14a5ed2665dcd1e256d4f9bd14a
mayyyitn <tnn03754@gmail.com>
2024-04-23 13:38:56 +0700

    DuyTan

--- DAO/LoginDAO.cs
+++ DAO/LoginDAO.cs
@@ -1,5 +1,6 @@
 ﻿using Project_JobApp.Classes;
 using Project_JobApp.Database;
+using Project_JobApp.Forms;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,6 +11,7 @@ using System.Windows.Forms;
 
 namespace Project_JobApp.DAO
 {
+    
     internal class LoginDAO
     {
         DBAccess dba = new DBAccess();
@@ -17,9 +19,23 @@ namespace Project_JobApp.DAO
 
         public bool CheckLogin(Account acc)
         {
-            string sqlStr = string.Format("select * from ACCOUNT where userid = '{0}' and matkhau = '{1}' and vaitro = '{2}'", acc.Userid, acc.Matkhau, acc.Vaitro);
-            DataTable dt = dba.ExecuteSelect(sqlStr);
-            return dt.Rows.Count > 0;
+            using (var db = new LoginDFContext())
+            {
+                var account = new ACCOUNT
+                {
+                    userid = acc.Userid,
+                    matkhau = acc.Matkhau,
+                    vaitro = acc.Vaitro,
+                };
+                var query = (from q in db.ACCOUNTs where q.userid == acc.Userid select q).ToList();
+
+                if (query.Count > 0) return true;
+                else return false;
+            }
+            //string sqlStr = string.Format("select * from ACCOUNT where userid = '{0}' and matkhau = '{1}' and vaitro = '{2}'", acc.Userid, acc.Matkhau, acc.Vaitro);
+            //DataTable dt = dba.ExecuteSelect(sqlStr);
+            //return dt.Rows.Count > 0;
         }
     }
+  
 }
